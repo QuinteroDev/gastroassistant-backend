ed398eccf65b7603fe1f08c9800d2dd7b224d702
Iván Ruiz <info@quintero.cz>
2025-06-06 12:51:38 +0200

    diseño renovado en la migración de expo a react cli

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lymbia.gastroassistant',
+  appName: 'GastroAssistant',
+  webDir: 'web-build'
+};
+
+export default config;
